8cdf3729badbc4973321c09d1c8fabb70b4707ea
Stephen polley <stephenHpolley@gmail.com>
2019-11-30 23:51:43 +0000

    File updated

--- app/src/main/java/com/example/bucketdrop/ActivityMain.java
+++ app/src/main/java/com/example/bucketdrop/ActivityMain.java
@@ -1,14 +1,31 @@
 package com.example.bucketdrop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initbackgoundImage();
+    }
+
+    private void initbackgoundImage(){
+        ImageView imageView = findViewById(R.id.imageViewbg);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(imageView);
     }
 }
